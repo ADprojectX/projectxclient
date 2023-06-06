48ddb3b6271dc3189962f48100a67159f1f9df40
AYUSH DAYANI <ayushdayani456@gmail.com>
2023-06-06 01:21:37 -0400

    time-based slider: non functional

--- src/videoplayer.js
+++ src/videoplayer.js
@@ -2,11 +2,37 @@ import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 import { Container, AppBar, Toolbar, Typography, Slider } from '@mui/material';
 
-const VPlayer = ({ videoSrc, audioSrc }) => {
+const VideoPlayer = ({ videoSrc, audioSrc, currentTime }) => {
+  const [seeking, setSeeking] = useState(false);
+
+  const handleProgress = (progress) => {
+    if (!seeking) {
+      currentTime(progress.playedSeconds);
+    }
+  };
+
+  const handleSeek = (event, newValue) => {
+    currentTime(newValue);
+    setSeeking(true);
+  };
+
+  const handleSeekEnd = () => {
+    setSeeking(false);
+  };
+
   return (
     <div>
       <Container maxWidth="md" height="md">
-        <ReactPlayer url={videoSrc} playing={true} controls={true} />
+        <ReactPlayer
+          url={videoSrc}
+          playing={true}
+          controls={true}
+          onProgress={handleProgress}
+          progressInterval={1000}
+          onSeek={handleSeek}
+          onSeekEnd={handleSeekEnd}
+          currentTime={currentTime}
+        />
         <audio id="audio" src={audioSrc} autoPlay />
       </Container>
     </div>
@@ -14,12 +40,28 @@ const VPlayer = ({ videoSrc, audioSrc }) => {
 };
 
 const SceneSelector = ({ scenes }) => {
-  const [currentScene, setCurrentScene] = useState(scenes[0]);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const handleTimeChange = (event, newValue) => {
+    setCurrentTime(newValue);
+  };
 
   const handleSceneSelect = (event, newValue) => {
-    const scene = scenes[newValue];
-    setCurrentScene(scene);
+    setCurrentTime(newValue);
   };
+
+  const getCurrentScene = () => {
+    for (let i = 0; i < scenes.length - 1; i++) {
+      const currentScene = scenes[i];
+      const nextScene = scenes[i + 1];
+      if (currentTime >= currentScene.time && currentTime < nextScene.time) {
+        return currentScene;
+      }
+    }
+    return scenes[scenes.length - 1];
+  };
+
+  const currentScene = getCurrentScene();
 
   return (
     <div>
@@ -29,33 +71,33 @@ const SceneSelector = ({ scenes }) => {
         </Toolbar>
       </AppBar>
       <Toolbar />
-      <VPlayer videoSrc={currentScene.video} audioSrc={currentScene.audio} />
+      <VideoPlayer videoSrc={currentScene.video} audioSrc={currentScene.audio} currentTime={setCurrentTime} />
       <Slider
-        defaultValue={0}
-        onChange={handleSceneSelect}
+        value={currentTime}
+        onChange={handleTimeChange}
+        onChangeCommitted={handleSceneSelect}
         min={0}
-        max={scenes.length - 1}
-        marks={scenes.map((scene, index) => ({ value: index, label: scene.name }))}
+        max={scenes[scenes.length - 1].time}
+        marks={scenes.map((scene) => ({ value: scene.time, label: scene.name }))}
         valueLabelDisplay="auto"
       />
     </div>
   );
 };
 
-const VideoPlayer = () => {
+const App = () => {
   const scenes = [
-    { name: 'Scene0', video: '/output/video0.mp4', audio: '/audio/voice#scene#0.mp3' },
-    { name: 'Scene1', video: '/output/video1.mp4', audio: '/audio/voice#scene#1.mp3' },
-    { name: 'Scene2', video: '/output/video2.mp4', audio: '/audio/voice#scene#2.mp3' },
-    { name: 'Scene3', video: '/output/video3.mp4', audio: '/audio/voice#scene#3.mp3' },
-    { name: 'Scene4', video: '/output/video4.mp4', audio: '/audio/voice#scene#4.mp3' },
+    { name: 'Scene0', video: '/output/video0.mp4', audio: '/audio/voice#scene#0.mp3', time: 0 },
+    { name: 'Scene1', video: '/output/video1.mp4', audio: '/audio/voice#scene#1.mp3', time: 10 },
+    { name: 'Scene2', video: '/output/video2.mp4', audio: '/audio/voice#scene#2.mp3', time: 20 },
+    { name: 'Scene3', video: '/output/video3.mp4', audio: '/audio/voice#scene#3.mp3', time: 30 },
+    { name: 'Scene4', video: '/output/video4.mp4', audio: '/audio/voice#scene#4.mp3', time: 40 },
     // Add more scenes here...
   ];
-
   return <SceneSelector scenes={scenes} />;
 };
 
-export default VideoPlayer;
+export default App;
 
 // const SceneSelector = ({ scenes }) => {
 //   const [currentScene, setCurrentScene] = useState(scenes[0]);
