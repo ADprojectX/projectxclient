f194d6ca992335bcd46010c04499deb2ff14b1ec
Aatish Dhami <32740715+Aatish-Dhami@users.noreply.github.com>
2023-08-10 04:49:07 -0700

    Update ProjectPage.js

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -7,17 +7,16 @@ import axios from 'axios';
 const API_BASE_URL = 'http://localhost:8000/api';
 const REQ_BASE_URL = 'http://localhost:8000/req';
 
-
 function ProjectPage() {
   
     const [projects, setProjects] = useState([]);
     const navigate = useNavigate();
 
-
     useEffect(() => {
         axios.get(`${REQ_BASE_URL}/get-project`, { withCredentials: true })
             .then(response => {
-                setProjects(response.data);  // assuming response data contains the list of projects
+                const sortedProjects = response.data.sort((a, b) => new Date(b.created) - new Date(a.created));  // sorting by date
+                setProjects(sortedProjects);
             })
             .catch(error => {
                 console.error('Failed to fetch projects:', error);
@@ -29,24 +28,37 @@ function ProjectPage() {
         navigate('/video');
     }
 
+    function handleCreateProject() {
+        navigate('/dashboard');
+    }
 
     return (
         <div className="project-page">
             <SideNavBar />
             <div className="project-page-main">
                 <h2>Projects</h2>
-                <div className="project-grid">
-                    {projects.map(project => (
-                        <div key={project.id} className="project-item" onClick={() => handleProjectClick(project.id)}>
-                            <p>{project.topic || "Untitled"}</p>
-                            {/* <h3>Project ID: {project.id}</h3> */}
-                            <p>Created: {new Date(project.created).toLocaleString()}</p>
+                {
+                    projects.length > 0 ?
+                    (
+                        <div className="project-grid">
+                            {projects.map(project => (
+                                <div key={project.id} className="project-item" onClick={() => handleProjectClick(project.id)}>
+                                    <p>{project.topic || "Untitled"}</p>
+                                    <p>Created: {new Date(project.created).toLocaleString()}</p>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
+                    ) :
+                    (
+                        <div className="no-projects">
+                            <p>No projects available.</p>
+                            <button onClick={handleCreateProject}>Create New Project</button>
+                        </div>
+                    )
+                }
             </div>
         </div>
     );
-  }
+}
 
-  export default ProjectPage;
+export default ProjectPage;
