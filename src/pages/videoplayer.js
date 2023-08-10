c88b0fb885cdb6fc01051192a49eb652614b2c28
Aatish Dhami <32740715+Aatish-Dhami@users.noreply.github.com>
2023-08-10 02:28:01 -0700

    loading skipped

--- src/pages/videoplayer.js
+++ src/pages/videoplayer.js
@@ -3,20 +3,70 @@ import { useLocation } from 'react-router-dom';
 import EditorMain from '../components/video-editing/Editormain';
 import SideNavBar from '../components/SideBar';
 import './css/VideoPlayer.css'
+import JSZip from 'jszip';
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:8000/api';
+const REQ_BASE_URL = 'http://localhost:8000/req';
 
 
 const VideoPlayer = () => {
   const [videoFiles, setVideoFiles] = useState([]);
-  
-  const location = useLocation();
-  let reqid = location.state && location.state.reqid
-  let videoData = location.state && location.state.videoFiles
 
+  const location = useLocation();
+  let reqid = location.state && location.state.reqid;
 
   useEffect(() => {
-    setVideoFiles(videoData)
-    console.log(videoFiles)
+    axios
+      .get(`${REQ_BASE_URL}/video-files/`, { responseType: 'arraybuffer' })
+      .then((response) => {
+        const zip = new JSZip();
+        return zip.loadAsync(response.data);
+      })
+      .then((zip) => {
+        const filePromises = Object.keys(zip.files).map((fileName) => {
+          const file = zip.files[fileName];
+          return file.async('blob').then((blob) => {
+            return { name: fileName, url: URL.createObjectURL(blob) };
+          });
+        });
+        return Promise.all(filePromises);
+      })
+      .then((videos) => {
+        const videoPromises = videos.map((video) => {
+          return new Promise((resolve) => {
+            const videoElement = document.createElement('video');
+            videoElement.src = video.url;
+            videoElement.addEventListener('loadedmetadata', () => {
+              const duration = videoElement.duration;
+              resolve({ ...video, duration });
+            });
+          });
+        });
+        return Promise.all(videoPromises);
+      })
+      .then((videosWithDuration) => {
+        const videoFiles = videosWithDuration.map((video, index) => ({
+          name: `Scene${index}`,
+          video: video.url,
+          audio: '', // Provide audio URL if available
+          duration: video.duration
+        }));
+        setVideoFiles(videoFiles); // Set the state here instead of navigating
+      })
+      .catch((error) => {
+        console.error('Failed to retrieve video files:', error);
+      });
   }, []);
+  
+  // let reqid = location.state && location.state.reqid
+  // let videoData = location.state && location.state.videoFiles
+
+
+  // useEffect(() => {
+  //   setVideoFiles(videoData)
+  //   console.log(videoFiles)
+  // }, []);
   
   return (
     <div className='video-page'>
