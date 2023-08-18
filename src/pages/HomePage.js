b883317834f4121974347830fe3d892c91f372a6
Aatish Dhami <32740715+Aatish-Dhami@users.noreply.github.com>
2023-08-18 16:55:12 -0700

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,21 +11,23 @@ function HomePage() {
     return (
       <div className="home">
         <PublicNavBar />
-        <div className='hero'>
-          <p className='hero-desc1'>Turn Your Vision into <span className='hero-top-title'>Reality</span></p>
-          <h1>Craft your videos with <span className='hero-title-gen-ai'>generative AI</span></h1>
-          <p className='hero-desc2'>Start with a thought, and let the AI evolve it. With scripts that engage, scenes that enthrall, voiceovers that connect, and edits that enchant. Welcome to impactful storytelling.</p>
-          <button>Try Now</button>
-        </div>
-        <div>
-          <div className="video-container">
-            <video width="100%" height="auto" autoPlay muted loop>
-              <source src="../main.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+        <div className='home-main'>
+          <div className='hero'>
+            <p className='hero-desc1'>Turn Your Vision into <span className='hero-top-title'>Reality</span></p>
+            <h1>Craft your videos with <span className='hero-title-gen-ai'>generative AI</span></h1>
+            <p className='hero-desc2'>Start with a thought, and let the AI evolve it. With scripts that engage, scenes that enthrall, voiceovers that connect, and edits that enchant. Welcome to impactful storytelling.</p>
+            <button>Try Now</button>
+          </div>
+
+          <div>
+            <div className="video-container">
+              <video width="100%" height="auto" autoPlay muted loop>
+                <source src="../main.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
           </div>
         </div>
-
       </div>
     );
 }
