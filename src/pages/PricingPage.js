749119303cf86ffa858a72ae7e8333a804d15de2
Aatish Dhami <32740715+Aatish-Dhami@users.noreply.github.com>
2023-09-19 01:06:20 -0700

    Update PricingPage.js

--- src/pages/PricingPage.js
+++ src/pages/PricingPage.js
@@ -11,8 +11,19 @@ const PricingPage = () => {
                 price="15.00" 
                 plan_name="Basic Plan"
             />
+            <PricingCard 
+                api_url="API_URL"
+                button_name="Checkout"
+                price="25.00" 
+                plan_name="Premium Plan"
+            />
+            <PricingCard 
+                api_url="API_URL"
+                button_name="Checkout"
+                price="10.00" 
+                plan_name="Credits"
+            />
         </div>
-
 	);
 };
 
